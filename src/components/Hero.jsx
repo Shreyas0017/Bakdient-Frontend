7bc0fb5f9292f72a220d2f1e4a67359250d399d4
Atanu2k4 <atanu.saha2004@gmail.com>
2025-04-06 22:50:47 +0530

    Add navigation functionality to Hero section and update button actions

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,14 @@
 import { ArrowRight, Info } from "lucide-react";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import { useNavigate } from "react-router-dom";
 
 const HeroSection = () => {
+  const navigate = useNavigate();
+
+  const handleGetStarted = () => {
+    navigate("/recipes");
+  };
+
   return (
     <section className="bg-[#F9EAE1] min-h-screen flex flex-col py-6 px-4 md:flex-row md:items-center md:justify-between md:px-8 lg:px-20 mt-0 md:mt-[-100px]">
       {/* Left Side: Text Content */}
@@ -20,7 +27,11 @@ const HeroSection = () => {
         </p>
         {/* Buttons */}
         <div className="mt-5 md:mt-6 flex flex-col sm:flex-row space-y-3 sm:space-y-0 sm:space-x-4">
-          <button className="px-4 py-2 md:px-5 md:py-3 bg-[#DC7F9B] text-white text-sm md:text-base rounded-full flex items-center justify-center gap-2 shadow-md hover:bg-[#c96e8a] transition">
+
+          <button
+            onClick={handleGetStarted}
+            className="px-4 py-2 md:px-5 md:py-3 bg-[#DC7F9B] text-white text-sm md:text-base rounded-full flex items-center justify-center gap-2 shadow-md hover:bg-[#c96e8a] transition"
+          >
             Get Started <ArrowRight className="w-4 h-4 md:w-5 md:h-5" />
           </button>
           <button className="px-4 py-2 md:px-5 md:py-3 border border-gray-500 text-gray-800 text-sm md:text-base rounded-full flex items-center justify-center gap-2 shadow-md hover:bg-gray-200 transition">
@@ -28,7 +39,6 @@ const HeroSection = () => {
           </button>
         </div>
       </div>
-      {/* Right Side: Lottie Animation */}
       <div className="flex justify-center items-center w-full order-1 md:order-2 md:w-1/2 h-64 sm:h-72 md:h-auto">
         <DotLottieReact
           src="https://lottie.host/fa570ae8-2d05-45d4-87b9-123c32cf476b/ycnbtAOtKa.lottie"
